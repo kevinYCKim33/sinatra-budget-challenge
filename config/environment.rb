402d09db1db3024cb34672d1e494875803dacc8a
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-01-05 20:24:21 -0800

    config postgress pw found

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,12 @@
-# ENV['SINATRA_ENV'] ||= "development"
+ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
+
+# ActiveRecord::Base.establish_connection(
+#   :adapter => "sqlite3",
+#   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+# )
 
 configure :development do
   ActiveRecord::Base.establish_connection(
@@ -11,7 +16,7 @@ configure :development do
 end
 
 configure :production do
-  db = URI.parse(ENV['HEROKU_POSTGRESQL_RED_URL'] || 'postgres://localhost/mydb')
+  db = URI.parse(ENV['HEROKU_POSTGRESQL_COBALT_URL'] || 'postgres://xejhvedmikhhzx:401fd65eb77d408dd4ba12a89a1936df2ca7138d07bf6a62c159a8173f8c420c@ec2-107-22-183-40.compute-1.amazonaws.com:5432/ddq7f3ul9uboem')
 
   ActiveRecord::Base.establish_connection(
     :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
@@ -22,5 +27,7 @@ configure :production do
     :encoding => 'utf8'
   )
 end
+
+require 'rack-flash'
 
 require_all 'app'
