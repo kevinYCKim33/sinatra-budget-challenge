b5d9ef9759bc3c0dd71afb289a73cba99d2ca65e
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-01-05 19:57:14 -0800

    revert it all back

--- Gemfile
+++ Gemfile
@@ -5,13 +5,8 @@ gem 'activerecord', :require => 'active_record'
 gem 'sinatra-activerecord', :require => 'sinatra/activerecord'
 gem 'rake'
 gem 'require_all'
-group :production do
-  gem 'pg'
-end
 
-# group :development do
-#   gem 'sqlite3'
-# end
+gem 'sqlite3'
 gem 'thin'
 gem 'shotgun'
 gem 'pry'
--- config/environment.rb
+++ config/environment.rb
@@ -1,70 +1,11 @@
-# ENV['SINATRA_ENV'] ||= "development"
-#
-# require 'bundler/setup'
-# Bundler.require(:default, ENV['SINATRA_ENV'])
-#
-# ActiveRecord::Base.establish_connection(
-#   :adapter => "sqlite3",
-#   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-# )
-#
-# require_all 'app'
-
-
-
-source 'http://rubygems.org'
-ruby '2.3.1'
-
-gem 'sinatra'
-gem 'activerecord', :require => 'active_record'
-gem 'sinatra-activerecord', :require => 'sinatra/activerecord'
-# gem 'sqlite3', :group => :development
-gem 'rake'
-gem 'require_all'
-gem 'thin'
-gem 'shotgun', :group => :development
-gem 'pry'
-gem 'bcrypt'
-gem "tux"
-gem 'rack-flash3'
-
-group :test do
-  gem 'rspec'
-  gem 'capybara'
-  gem 'rack-test'
-  gem 'database_cleaner', git: 'https://github.com/bmabey/database_cleaner.git'
-end
-
-group :production do
-  gem 'pg'
-  gem 'rails_12factor'
-end
+ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler/setup'
-require 'rack-flash'
+Bundler.require(:default, ENV['SINATRA_ENV'])
 
-Bundler.require
-
-configure :development do
-  ENV['SINATRA_ENV'] ||= "development"
-
-  ActiveRecord::Base.establish_connection(
-     :adapter => "sqlite3",
-     :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-   )
-end
-
-configure :production do
-  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
-
-  ActiveRecord::Base.establish_connection(
-     :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-     :host     => db.host,
-     :username => db.user,
-     :password => db.password,
-     :database => db.path[1..-1],
-     :encoding => 'utf8'
-  )
-end
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+)
 
 require_all 'app'
